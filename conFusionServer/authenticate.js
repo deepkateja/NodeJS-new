0b4679d53d29f5601c7c37a3ed108c399cf16e3e
Deep Kateja <kd.kateja@gmail.com>
2021-01-04 00:38:03 +0530

    Assignment 3

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken');
 
 var config = require('./config');
 
-passport.use(new LocalStrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -34,4 +34,32 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
             });
         }));
 
-exports.verifyUser = passport.authenticate('jwt',{session:false});
+        exports.verifyUser = function (req, res, next) {
+            var token = req.body.token || req.query.token || req.headers['x-access-token'];
+            if (token) {
+                jwt.verify(token, config.secretKey, function (err, decoded) {
+                    if (err) {
+                        var err = new Error('You are not authenticated!');
+                        err.status = 401;
+                        return next(err);
+                    } else {
+                        req.decoded = decoded;
+                        next();
+                    }
+                });
+            } else {
+                var err = new Error('No token provided!');
+                err.status = 403;
+                return next(err);
+            }
+        };
+        
+        exports.verifyAdmin = function (req, res, next) {
+            if (req.user.admin) {
+                next();
+            } else {
+                var err = new Error('You are not authorized to perform this operation!');
+                err.status = 403;
+                return next(err);
+            }
+        }
